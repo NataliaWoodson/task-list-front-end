0dcff28fad58a4661bb181802e11013e290c80ff
Natalia <natalia.woodson@gmail.com>
2022-06-18 14:07:21 -0400

    completed wave 2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
-
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+import TaskData from './tasks.json';
+import { useState } from 'react';
 
 const App = () => {
+  const [taskData, setTaskData] = useState(TaskData);
+
+  const toggleComplete = (id) => {
+    const newTaskData = TaskData.map((task) => {
+      if (task.id === id) {
+        return { ...task, isComplete: (task.isComplete = !task.isComplete) };
+      } else {
+        return task;
+      }
+    });
+    setTaskData(newTaskData);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>
+          <TaskList tasks={taskData} setComplete={toggleComplete} />
+        </div>
       </main>
     </div>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,17 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
-  const [complete, setComplete] = useState(isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+// const Task = ({ id, title, isComplete }) => {
+const Task = ({ id, title, isComplete, setComplete }) => {
+  const buttonClass = isComplete ? 'tasks__item__toggle--completed' : '';
 
+  const markComplete = () => {
+    setComplete(id);
+  };
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        onClick={markComplete}
       >
         {title}
       </button>
@@ -24,6 +27,7 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  setComplete: PropTypes.func.isRequired,
 };
 
 export default Task;
