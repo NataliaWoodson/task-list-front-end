4b51d1cf720a1a6e3908fcad44273a94d9b1fa01
Tyrah <tyrah96@gmail.com>
2022-06-26 12:26:53 -0700

    merge conflict incoming

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 
 import './Task.css';
 
-// const Task = ({ id, title, isComplete }) => {
 const Task = ({ id, title, isComplete, setComplete, deletetask }) => {
   const buttonClass = isComplete ? 'tasks__item__toggle--completed' : '';
 
